3f8fcb6b6cd1538e320b3d72201f808ea7000b45
jobrane ben salah <83889965+jobrane33@users.noreply.github.com>
2024-06-25 21:16:35 +0100

    Revert "final work jobrane"

--- src/app/demo/components/auth/forgotpassword/forgotpassword.module.ts
+++ src/app/demo/components/auth/forgotpassword/forgotpassword.module.ts
@@ -5,22 +5,14 @@ import { ForgotPasswordComponent } from './forgotpassword.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { AppConfigModule } from 'src/app/layout/config/app.config.module';
-import { FormsModule } from '@angular/forms';
-import { RouterModule,Routes } from '@angular/router';
-import { ToastModule } from 'primeng/toast';
-const routes: Routes = [
-    { path: ':uuid', component: ForgotPasswordComponent }
-  ];
+
 @NgModule({
     imports: [
         CommonModule,
         ButtonModule,
-        FormsModule,
         InputTextModule,
         ForgotPasswordRoutingModule,
-        AppConfigModule,
-        ToastModule,
-        RouterModule.forChild(routes)
+        AppConfigModule
     ],
     declarations: [ForgotPasswordComponent]
 })
